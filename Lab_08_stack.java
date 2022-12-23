3eac17d72ab4ce42386c05dd7a6102b22f1893de
gauravxta <87366731+gauravxta@users.noreply.github.com>
2022-12-23 04:25:27 +0000

    pop function added

--- Lab_08_stack.java
+++ Lab_08_stack.java
@@ -11,5 +11,35 @@ class CreateStack {
         top = -1;
     }
 
-   
+     // the stack is empty or not  
+  void isEmpty() {
+    if(top == -1) {
+      System.out.println("Stack is empty.");
+    } else {
+      System.out.println("Stack is not empty.");
+    }
+  }
+
+    void push(int x)
+    {
+        if(top == (max - 1))
+        {
+            System.out.println("Stack size limit reached.");
+
+        }
+        else 
+        {
+            stack[++top] = x;
+            System.out.println(x + " is added into the stack.");
+        }
+    }
+}
+
+public class Lab_08_stack {
+    public static void main (String[] args)
+    {
+        CreateStack stack = new stack();
+
+        stack.pop(3);
+    }
 }
