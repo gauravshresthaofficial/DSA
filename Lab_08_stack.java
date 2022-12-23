2950d9c12f651bfe8136ae870cc82d19fb3e7d48
gauravxta <87366731+gauravxta@users.noreply.github.com>
2022-12-23 04:44:31 +0000

    push, pop, display operation

--- Lab_08_stack.java
+++ Lab_08_stack.java
@@ -12,12 +12,11 @@ class CreateStack {
     }
 
      // the stack is empty or not  
-  void isEmpty() {
-    if(top == -1) {
-      System.out.println("Stack is empty.");
-    } else {
-      System.out.println("Stack is not empty.");
-    }
+  boolean isEmpty() {
+    if(top == -1)
+        return true;
+    else
+        return false;
   }
 
     void push(int x)
@@ -33,13 +32,48 @@ class CreateStack {
             System.out.println(x + " is added into the stack.");
         }
     }
+
+    void pop()
+    {
+        if(isEmpty())
+        {
+            System.out.println("Stack is empty. Deletion not possible.");
+        }
+        else{
+            int x = stack[top--];
+            System.out.println(x + " is deleted from the stack.");
+        }
+
+    }
+
+    void Display()
+    {
+        if(isEmpty())
+        {
+            System.out.println("Stack is empty.");
+        }
+        else 
+        {
+            while (top != -1)
+            {
+                System.out.print(stack[top]);
+                top--;
+            }
+        }
+        
+    }
+
+
 }
 
 public class Lab_08_stack {
     public static void main (String[] args)
     {
-        CreateStack stack = new stack();
+        CreateStack stack = new CreateStack();
 
-        stack.pop(3);
+        stack.push(3);
+        stack.push(4);
+        stack.pop();
+        stack.Display();
     }
 }
